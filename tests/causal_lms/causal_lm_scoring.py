76e31cc2a6f4a276764d266709b30d309006ef5e
cromac <clement.romac@gmail.com>
2024-02-21 09:30:55 +0100

    Properly testing batched scoring

--- tests/causal_lms/causal_lm_scoring.py
+++ tests/causal_lms/causal_lm_scoring.py
@@ -3,6 +3,8 @@ import torch
 import hydra
 import numpy
 
+from torch.nn.functional import log_softmax
+
 from lamorel import Caller, BaseModuleFunction, lamorel_init
 
 lamorel_init()
@@ -22,13 +24,15 @@ class LogScoringModuleFn(BaseModuleFunction):
                 end_of_context_position = 0
             else:  # hence input should be removed from result
                 end_of_context_position = len(
-                    tokenized_contexts[0]["input_ids"]) # inputs are padded so all of same size
+                    tokenized_contexts[0]["input_ids"])  # inputs are padded so all of same size and last token was sent to outputs (so no -1)
 
             logits = forward_outputs["logits"][:, end_of_context_position:-1, :]
             output_tokens = minibatch["input_ids"][:, end_of_context_position+1:]
         else:
             logits = forward_outputs["logits"][:, :-1, :]  # skip </s> token appended by tokenizer
             output_tokens = minibatch["decoder_input_ids"][:, 1:]  # skip pad token
+
+        logits = log_softmax(logits, dim=-1)
 
         tokens_logprobs = \
             torch.gather(logits, 2, output_tokens[:, :, None]).squeeze(-1).to(torch.float32)  # filter with sequence tokens
@@ -51,7 +55,32 @@ class CausalLMScoring(unittest.TestCase):
 
     def test_vanilla_scoring(self):
         global lm_server
-        generated = lm_server.generate(contexts=self.PROMPTS, num_return_sequences=3, return_logprobs=True, do_sample=True)
+        generated = lm_server.generate(contexts=self.PROMPTS, num_return_sequences=3, return_logprobs=True,
+                                       do_sample=True, top_k=None, max_new_tokens=5, temperature=1, top_p=1.0)
+        scores = []
+        for idx, _prompt in enumerate(self.PROMPTS):
+            _scores = lm_server.custom_module_fns(['score'],
+                                                contexts=[_prompt],
+                                                candidates=[
+                                                    [_text['text'] for _text in generated[idx]]
+                                                ])
+
+            scores.extend([_s['score'].numpy() for _s in _scores])
+
+        scores = numpy.array(scores)
+        generated_scores = numpy.array([
+            [_text['text_logprob'] for _text in _result] for _result in generated
+        ])
+        try:
+            numpy.testing.assert_array_almost_equal(scores, generated_scores, 1)
+        except AssertionError as e:
+            self.fail()
+
+    def test_batched_scoring(self):
+        global lm_server
+        generated = lm_server.generate(contexts=self.PROMPTS, num_return_sequences=3, return_logprobs=True,
+                                        do_sample=True, top_k=None, max_new_tokens=5, temperature=1, top_p=1)
+
         _scores = lm_server.custom_module_fns(['score'],
                                             contexts=self.PROMPTS,
                                             candidates=[
@@ -61,10 +90,12 @@ class CausalLMScoring(unittest.TestCase):
 
         scores = numpy.array([_s['score'].numpy() for _s in _scores])
         generated_scores = numpy.array([
-            [_text['score'] for _text in _result] for _result in generated
+            [_text['text_logprob'] for _text in _result] for _result in generated
         ])
-        numpy.testing.assert_array_almost_equal(scores, generated_scores, 1)
-
+        try:
+            numpy.testing.assert_array_almost_equal(scores, generated_scores, 1)
+        except AssertionError as e:
+            self.fail()
 
 @hydra.main(config_path='config', config_name='config')
 def main(config_args):
